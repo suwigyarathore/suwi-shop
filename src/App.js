b736af859af571a09e96b4b84c0c3d94cf0ed1b8
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-11-16 16:20:49 +0100

    Adding collection item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-function App() {
+const HatsPage = props => {
   return (
     <div>
-      <HomePage></HomePage>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+function App(props) {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/hats" component={HatsPage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+export default class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
