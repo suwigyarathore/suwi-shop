aa5f85bf3327b174fc10bde38d49a832d203c5f2
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-12-12 16:15:21 +0100

    Adding custom form

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import "./App.css";
 
-const HatsPage = props => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
-
-function App(props) {
+function App() {
   return (
     <div>
       <Header></Header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/hats" component={HatsPage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
